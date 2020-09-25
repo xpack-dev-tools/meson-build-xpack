43ce05cd1f28225116519a255e6503f670f36097
Liviu Ionescu <ilg@livius.net>
2020-09-25 17:06:03 +0300

    VERSION 0.55.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 Releases in reverse chronological order.
 
+### 2020-09-25
+
+- v0.55.3-1 created
+
 ### 2020-08-23
 
 - v0.55.1-1 created
--- scripts/common-versions-source.sh
+++ scripts/common-versions-source.sh
@@ -25,56 +25,27 @@ function build_versions()
   README_OUT_FILE_NAME=${README_OUT_FILE_NAME:-"README-${RELEASE_VERSION}.md"}
 
   # Keep them in sync with combo archive content.
-  if [[ "${RELEASE_VERSION}" =~ 0\.55\.1-* ]]
+  if [[ "${RELEASE_VERSION}" =~ 0\.55\.3-* ]]
   then
 
     # -------------------------------------------------------------------------
 
     PYTHON_VERSION="3.8.5"
   
-    if [ "${TARGET_PLATFORM}" != "win32" ]
-    then
-
-      build_zlib "1.2.8"
-
-      build_libmpdec "2.5.0"
-
-      build_expat "2.2.9"
-
-      build_libffi "3.3"
-
-      build_libxcrypt "4.4.17"
-      
-      build_python3 "${PYTHON_VERSION}"
-    fi
-
     if [ "${TARGET_PLATFORM}" == "win32" ]
     then
       download_python3_win "${PYTHON_VERSION}"
+    else
+      build_zlib "1.2.8"
+      build_libmpdec "2.5.0"
+      build_expat "2.2.9"
+      build_libffi "3.3"
+      build_libxcrypt "4.4.17"
 
-      # Copy the .zip with the entire Python run-time (.pyc files).
-      mkdir -pv "${APP_PREFIX}/bin"
-      cp -v "${SOURCES_FOLDER_PATH}/${PYTHON3_WIN_EMBED_FOLDER_NAME}/python${PYTHON3_VERSION_MAJOR_MINOR}.zip" \
-        "${APP_PREFIX}/bin"
-      cp -v "${SOURCES_FOLDER_PATH}/${PYTHON3_WIN_EMBED_FOLDER_NAME}/python.exe" \
-        "${APP_PREFIX}/bin/python-meson.exe"
-
-      mkdir -pv "${APP_PREFIX}/bin/DLLs"
-      # Copy the Windows specific DLLs (.pyd) to the separate folder;
-      # they are dynamically loaded by Python.
-      cp -v "${SOURCES_FOLDER_PATH}/${PYTHON3_WIN_EMBED_FOLDER_NAME}"/*.pyd "${APP_PREFIX}/bin/DLLs"
-      # Copy the usual DLLs too.
-      cp -v "${SOURCES_FOLDER_PATH}/${PYTHON3_WIN_EMBED_FOLDER_NAME}"/*.dll "${APP_PREFIX}/bin/DLLs"
-
-      # From bin are copied dependencies.
-      cp -v "${SOURCES_FOLDER_PATH}/${PYTHON3_WIN_EMBED_FOLDER_NAME}"/*.dll "${LIBS_INSTALL_FOLDER_PATH}/bin"
-
-      cp -v "${BUILD_GIT_PATH}/extras/includes/pyconfig-${PYTHON_VERSION}.h" \
-        "${SOURCES_FOLDER_PATH}/${PYTHON3_SRC_FOLDER_NAME}/Include/pyconfig.h"
-
+      build_python3 "${PYTHON_VERSION}"
     fi
 
-    build_meson "0.55.1"
+    build_meson "0.55.3"
 
     # -------------------------------------------------------------------------
   else
