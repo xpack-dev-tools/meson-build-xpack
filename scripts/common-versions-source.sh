bcea8c658a9e19530e5fc2abb69a49a75992745b
Liviu Ionescu <ilg@livius.net>
2020-09-26 14:18:38 +0300

    common-versions: add more python libs

--- scripts/common-versions-source.sh
+++ scripts/common-versions-source.sh
@@ -36,11 +36,23 @@ function build_versions()
     then
       download_python3_win "${PYTHON_VERSION}"
     else
+      # On macOS, to prevent Python picking system libraries,
+      # provide controlled versions of them.
       build_zlib "1.2.8"
+      build_bzip2 "1.0.8"
+      build_xz "5.2.5"
+
       build_libmpdec "2.5.0"
       build_expat "2.2.9"
       build_libffi "3.3"
+
       build_libxcrypt "4.4.17"
+      build_openssl "1.1.1h"
+
+      build_ncurses "6.2"
+      build_readline "8.0" # ncurses
+
+      build_sqlite "3.32.3"
 
       build_python3 "${PYTHON_VERSION}"
     fi
