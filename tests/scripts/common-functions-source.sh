f8196badfd42edd97bcd0db33c7299149c6963df
Liviu Ionescu <ilg@livius.net>
2020-09-27 11:40:59 +0300

    add test scripts

---
+++ tests/scripts/common-functions-source.sh
@@ -0,0 +1,25 @@
+# -----------------------------------------------------------------------------
+# This file is part of the xPack distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2020 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software 
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Common functions used in various tests.
+#
+# Requires 
+# - app_folder_path
+# - test_folder_path
+# - archive_platform (win32|linux|darwin)
+
+# -----------------------------------------------------------------------------
+
+function run_tests()
+{
+  time run_app "${app_folder_path}/bin/meson" --version
+}
+
+# -----------------------------------------------------------------------------
